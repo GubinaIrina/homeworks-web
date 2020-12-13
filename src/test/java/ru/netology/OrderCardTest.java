b79a4e334ceee36fd7a016d2db2a9da78c7ccfdb
Gubina Irina <irina.gubina98@mail.ru>
2020-12-13 18:37:47 +0300

    Добавлены тесты с невалидными данными

--- src/test/java/ru/netology/OrderCardTest.java
+++ src/test/java/ru/netology/OrderCardTest.java
@@ -23,4 +23,52 @@ public class OrderCardTest {
 
         $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+
+    @Test
+    void shouldSubmitRequestByName() {
+        open("http://localhost:9999");
+
+        SelenideElement form = $("form");
+        form.$("[data-test-id='name'] input").setValue("Vasiliy Иванов");
+        form.$("[data-test-id='phone'] input").setValue("+79270000000");
+        form.$("[data-test-id='agreement']").click();
+        form.$("button").click();
+
+        $("span.input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldSubmitRequestByPhone() {
+        open("http://localhost:9999");
+
+        SelenideElement form = $("form");
+        form.$("[data-test-id='name'] input").setValue("Василий Иванов");
+        form.$("[data-test-id='phone'] input").setValue("+792700000001");
+        form.$("[data-test-id='agreement']").click();
+        form.$("button").click();
+
+        $("[data-test-id='phone'] span.input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldSubmitRequestByCheckBox() {
+        open("http://localhost:9999");
+
+        SelenideElement form = $("form");
+        form.$("[data-test-id='name'] input").setValue("Василий Иванов");
+        form.$("[data-test-id='phone'] input").setValue("+79270000000");
+        form.$("button").click();
+
+        $(".input_invalid>.checkbox_box");
+    }
+
+    @Test
+    void shouldSubmitRequestByEmptyFields() {
+        open("http://localhost:9999");
+
+        SelenideElement form = $("form");
+        form.$("button").click();
+
+        $("span.input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
 }
